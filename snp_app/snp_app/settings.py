eba65dc28649fd68a4f657d29aa04e2f31cc765b
hyogeun <yhdok1110@naver.com>
2023-10-15 19:57:40 +0900

    commit

--- snp_app/snp_app/settings.py
+++ snp_app/snp_app/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 ]
 # CORS 추가
 CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1:8001')
+    'http://127.0.0.1:8000', 'http://localhost:3000')
 CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = "snp_app.urls"
 
